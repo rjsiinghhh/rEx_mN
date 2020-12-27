49d387b2d1607f38e62ca6764cd244e7617f11cd
rjsiinghhh <rjsiinghhh@gmail.com>
2020-12-27 01:20:14 -0800

    more

--- rex/src/places/pages/NewPlace.js
+++ rex/src/places/pages/NewPlace.js
@@ -6,8 +6,14 @@ import './NewPlace.css';
 const NewPlace = () => {
   return(
     <form className="place-form">
-      <Input element="input" type="text" label="Title" validators={()} onInput={} />
-    </form>
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[]}
+        errorText="Please Enter A Valid Title."
+        />
+      </form>
   );
 };
 
