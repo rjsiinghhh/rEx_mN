f1b5098ee156d21eb9b6d8c3b91e2d2ebcbbe9d1
rjsiinghhh <rjsiinghhh@gmail.com>
2020-12-26 23:00:25 -0800

    9

--- rex/src/places/pages/UserPlaces.js
+++ rex/src/places/pages/UserPlaces.js
@@ -1,42 +1,41 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 import PlaceList from '../components/PlaceList';
-
-
 
 const DUMMY_PLACES = [
   {
     id: 'p1',
-    title: 'Eiffel Tower',
-    description: 'Super pointy, Don\'t run with this in your hand, SpiderMan climbed up this one time, had a hard time getting down because there was nothing to web to.',
-    imageURL: 'https://i.imgur.com/Tw44FsT.ppg',
-    address: 'Champ de Mars, 5 Avenue Anatole France, 75007 Paris, France',
-    location: {
-      lat: 48.858093,
-      lng: 2.294694
-    },
-    creator: 'u1'
-  },
-  {
-    id: 'p2',
     title: 'Empire State Building',
-    description: 'One of the worlds most famous sky scraper. Honestly, it kinda sucks. lol One time, I dropped a penny off the top. Damn I don\'t think this is the place I should be saying this. Also, Spiderman also did some amazing life saving work up here. Hometown for him, so it\'s the home court advantage. Beatiful site to witness.. I wasn\'t there but from what they said it was amazing.',
-    imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    description: 'One of the worlds most famous sky scrapers. Honestly, it kinda sucks. lol One time, I dropped a penny off the top. Damn I do not think this is the place I should be saying this. Also, Spiderman also did some amazing work up here. Hometown for him, all about that home court advantage, yaddaamean?!. It was beatiful site to witness that day.. I was not there, but thats what I heard from TMZ.',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
     address: '20 W 34th St, New York, NY 10001',
     location: {
       lat: 40.7484405,
       lng: -73.9878584
     },
+    creator: 'u1'
+  },
+  {
+    id: 'p2',
+    title: 'Eiffel Tower',
+    description: 'Super pointy, Do not run with this in your hand, SpiderMan climbed up this one time, had a hard time getting down because there was nothing to web to.',
+    imageUrl:
+      'https://media.architecturaldigest.com/photos/5ef5f6b4e5c8c1d259c3b00b/1:1/w_1024%2Cc_limit/GettyImages-803432314.jpg',
+    address: 'Champ de Mars, 5 Avenue Anatole France, 75007 Paris, France',
+    location: {
+      lat: 48.858093,
+      lng: 2.294694
+    },
     creator: 'u2'
   }
 ];
 
-
 const UserPlaces = () => {
-
-  return <PlaceList items={DUMMY_PLACES} />;
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
 };
-
-
 
 export default UserPlaces;
